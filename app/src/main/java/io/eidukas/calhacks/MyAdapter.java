565beedec4a9e2e3877fbd773fb1d1904291196a
daniel <danielwzou@gmail.com>
2016-11-12 12:21:01 -0800

    Working on RecyclerView

---
+++ app/src/main/java/io/eidukas/calhacks/MyAdapter.java
@@ -0,0 +1,51 @@
+package io.eidukas.calhacks;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by daniel on 11/12/16.
+ */
+
+public class MyAdapter extends android.support.v7.widget.RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    String[] mDataSet;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    public MyAdapter() {
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        ViewHolder vh = new ViewHolder((TextView)v);
+        return vh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textView.setText(mDataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+
+
+
+}
