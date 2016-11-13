f2eeb1068bee1ca63096be59a9910454d0943961
junghhwang <jh5@illinois.edu>
2016-11-12 21:06:27 -0600

    in the middle of changing the lisitems into recipes

--- app/src/main/java/io/eidukas/calhacks/DetailsActivity.java
+++ app/src/main/java/io/eidukas/calhacks/DetailsActivity.java
@@ -1,19 +1,41 @@
 package io.eidukas.calhacks;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+
+import io.eidukas.calhacks.DataModels.Recipe;
+
+/**
+ * Created by daniel on 11/12/16.
+ */
 
 public class DetailsActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private ListView listView;
     private MyAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
     private String foodQuery;
 
     @Override
@@ -21,39 +43,43 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new MyAdapter();
-        recyclerView.setAdapter(adapter);
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Recipe recipe = adapter.getItem(position);
 
-        FoodSearcher foodSearcher = new FoodSearcher();
+                Intent intent = new Intent(parent.getContext(), RecipeActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, recipe);
+
+                // Attempt to start an activity that can handle the Intent
+                startActivity(intent);
+            }
+        });
+
+        adapter = new MyAdapter(this, R.layout.list_item);
+        listView.setAdapter(adapter);
+
+        SearchFoodTask task = new SearchFoodTask();
         foodQuery = getIntent().getStringExtra("args");
-        foodSearcher.execute(SPOONACULAR_SEARCH_URL + "/search?query=" + foodQuery + "&mashape-key=" + APIKey.getRecipeKey(this));
+        task.execute(SPOONACULAR_SEARCH_URL + "/search?query=" + foodQuery + "&mashape-key=" + R.raw.recipe);
     }
 
     private static final String SPOONACULAR_SEARCH_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes";
 
-    public class FoodSearcher extends AsyncTask<String, Integer, Map<String,Integer>> {
+    public class SearchFoodTask extends AsyncTask<String, Integer, Recipe[]> {
 
         @Override
-        protected Map<String, Integer> doInBackground(String... strings) {
-            return io.eidukas.calhacks.FoodSearcher.getFrequencyMap(foodQuery, APIKey.getRecipeKey(getApplicationContext()));
+        protected Recipe[] doInBackground(String... strings) {
+            return FoodSearcher.getDifferentRecipes(strings[0]);
         }
 
         @Override
-        protected void onPostExecute(Map<String, Integer> map) {
-            String[][] result = new String[map.size()][2];
-            int i = 0;
-            for (String key : map.keySet()) {
-                result[i][0] = key;
-                result[i][1] = Integer.toString(map.get(key));
-                i++;
-            }
-            adapter = new MyAdapter(result);
-            recyclerView.setAdapter(adapter);
-            super.onPostExecute(map);
+        protected void onPostExecute(Recipe[] recipes) {
+            adapter.clear();
+            adapter.addAll(recipes);
+            listView.setAdapter(adapter);
+            super.onPostExecute(recipes);
         }
     }
 
